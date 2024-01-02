d36792926631cbcec7ba714c693d3b22088cc5b3
abhi221055 <154426044+abhi221055@users.noreply.github.com>
2024-01-02 10:38:04 +0000

    let var and const done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,14 @@
 const accountId = 144553
-let accountEmail = "abhilash@gmail.com"
-var accountPass = "12345"
+let accEmail = "abhilas@gmail.com"
+var accPass = "1234565"
 accountCity = "Shimla"
-
-// accountId = 2 // not allowed
- 
+let accState;
+// accountId = 12// not allowed as constant can not be changed 
+accEmail ="ab@ab.com"
+accPass = "2121"
+accountCity = "Jaipur"
 console.log(accountId);
+/*  ab var ko use nahi karte hai ab sirf let use karte hai beacuse of scope issue
+console.table se tabular form me data aa jata hai. */
+console.table([accountId,accEmail,accPass,accountCity,accState])
 
