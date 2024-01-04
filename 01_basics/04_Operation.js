c60e969516127ed9b8fa0ba3dc0ccbe9af0ac69f
abhi221055 <154426044+abhi221055@users.noreply.github.com>
2024-01-04 09:45:48 +0000

    oerations and prefix and postfix operators.

---
+++ 01_basics/04_Operation.js
@@ -0,0 +1,37 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2*2)
+// console.log(2%2)
+// console.log(2**3)
+let str1 = "Hello"
+let str2 = " Abhilash"
+let str3 = str1+str2
+console.log(str3)
+console.log("1"+2);
+console.log(1+"2"); //yaha 1 number data type hai or ek string to ye add ni hue 
+console.log("1"+2+2) //yaha b pehle string tha to ye add ni ho paaya 
+console.log(1+4+"2"); //yaha pehle dono datatype number the is lie pehle add hue us k bad string data type add hua
+console.log(2+"3"+2) //yaha b string bich me aagya is lie hi add nahi hua.
+console.log(true)
+console.log(+true) //yaha ye boolean value le li 1 
+let num1,num2,num3
+num1= num2 = num3 = 4
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+// Prefix and Post fix operator read from mdn reference.
+let x = 3;
+const y = x++ //ye post fix hai yaha y to 3 hi rahega or x p increment karega
+console.log(y)
+console.log(x)
+console.log(x)
+// ye prefix ka example
+let a = 4;
+const b = ++a;
+console.log(a) //is me pehle ye a ki value ko increment kar dega or bad me increment wali hi value dikhayega a ki
+// post wale m pehli value fix rahegi or dursi value badegi or prefix me dono values sath sath badegi
+console.log(b)
