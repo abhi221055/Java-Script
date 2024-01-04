1cc37e3b02b21d499766ecbda29d8d177ba60498
abhi221055 <154426044+abhi221055@users.noreply.github.com>
2024-01-04 11:23:32 +0000

    comparison of data types

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+// console.log("2">1); //data type same hona chahye comparison k lie 
+// console.log("02">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+
+
+
+//strict check === is ka mtlb hai ki ye strictly check karta hai mtlb data type se b check krega.
+console.log("2"=== 2); //yaha isne data type se check kia.
