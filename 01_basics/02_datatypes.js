8b9556b58dfaa1e326db9cd9336ba15c04e60d3b
abhi221055 <154426044+abhi221055@users.noreply.github.com>
2024-01-04 07:52:56 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+// readability matters in coding//
+ let name = "abhilash"
+ let age = 23
+ let isloggedin = true
+ console.log(typeof(age))
+ console.log(typeof(null)) 
+
+// there are 7 data types in javascript
+// Number is ki max value hai => 2 to power 53
+// Big Int
+// String => "", '' 
+// Boolean => true/false
+// Null => ye ek stand alone value hai
+// [Ye ek object hai.]
+//Undefined => jab hum simple variable declare kar k value na de tab 
+// Symbol => ye hota hai uniqness find karne k lie ye mainly react me use hota hai.
+// objects b ek data type hai jisko hum ache se karenge aage.
+
